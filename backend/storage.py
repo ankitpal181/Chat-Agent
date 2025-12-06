300f7cc309fd79af9a13ca0c4fdda39725b502f7
Ankit Pal <ankitpal181@gmail.com>
2025-12-07 01:32:56 +0530

    mongo and postgres database storages tested and fixed

--- backend/storage.py
+++ backend/storage.py
@@ -1,8 +1,9 @@
-import sqlite3, os
+import sqlite3, os, psycopg
+from pymongo import MongoClient
 from typing import Literal
 from langgraph.checkpoint.sqlite import SqliteSaver
-# from langgraph.checkpoint.mongodb import MongoDBSaver
-# from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.checkpoint.memory import InMemorySaver
 
 
@@ -12,20 +13,20 @@ class Storage:
         mode: Literal["memory", "database"],
         database: Literal["sqlite", "mongo", "postgres"] = "sqlite"
     ) -> None:
-        if mode == "memory":
-            self.storage = InMemorySaver()
+        if mode == "memory": self.storage = InMemorySaver()
         else:
-            # if database == "mongo": self.storage = self._set_mongo_storage()
-            # if database == "postgres": self.storage = self._set_postgres_storage()
-            # else: self.storage = self._set_sqlite_storage()
-            self.storage = self._set_sqlite_storage()
+            if database == "mongo": self.storage = self._set_mongo_storage()
+            elif database == "postgres": self.storage = self._set_postgres_storage()
+            else: self.storage = self._set_sqlite_storage()
     
     def _set_sqlite_storage(self) -> SqliteSaver:
         connection = sqlite3.connect("chatbot.db", check_same_thread=False)
         return SqliteSaver(connection)
     
-    # def _set_mongo_storage(self) -> MongoDBSaver:
-    #     return MongoDBSaver(uri=os.environ.get("MONGODB_CONNECTION_URI", ""), db_name="chatbot.db")
+    def _set_mongo_storage(self) -> MongoDBSaver:
+        connection = MongoClient(os.environ.get("MONGODB_CONNECTION_URI"))
+        return MongoDBSaver(client=connection)
     
-    # def _set_postgres_storage(self) -> PostgresSaver:
-    #     return PostgresSaver(conn_string=os.environ.get("POSTGRES_CONNECTION_URI", ""))
+    def _set_postgres_storage(self) -> PostgresSaver:
+        connection = psycopg.connect(os.environ.get("POSTGRES_CONNECTION_URI"))
+        return PostgresSaver(connection)
